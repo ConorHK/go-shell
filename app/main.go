a36691e6f7293b15e2df55a5ceae4dd2d08e657c
Conor Knowles <dev@conorknowles.com>
2025-06-13 17:27:48 +0100

    Add repl

--- app/main.go
+++ app/main.go
@@ -9,14 +9,21 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+func evaluate() (output string) {
+}
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println("Error")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error")
+		}
+
+		if input == "exit" {
+			break
+		}
+		var output = command[:len(command)-1] + ": command not found"
+		fmt.Println(output)
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
